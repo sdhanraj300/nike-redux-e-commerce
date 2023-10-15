2341c50d146173799274c45046010de1ccb6f132
Dhanraj Singh <sdhanraj300@gmail.com>
2023-10-16 01:40:50 +0530

    added card of the shoes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { Hero } from "./components";
-import { heroapi } from "./data/data";
+import { Hero, Sales } from "./components";
+import { heroapi, popularsales, toprateslaes } from "./data/data";
+
 const App = () => {
   return (
-    <div className="">
-      <Hero heroapi={heroapi}   />
+    <div className="flex flex-col gap-16 relative">
+      <Hero heroapi={heroapi} />
+      <Sales endpoint={popularsales} />
+      <Sales endpoint={toprateslaes} />
     </div>
   );
 };
