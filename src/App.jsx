d7100134f64b18723c6a8221aa59c2ddf575e2d5
Dhanraj Singh <sdhanraj300@gmail.com>
2023-10-16 23:32:46 +0530

    added highlight component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React from "react";
-import { Hero, Sales } from "./components";
-import { heroapi, popularsales, toprateslaes } from "./data/data";
+import { FlexContent, Hero, Sales } from "./components";
+import {
+  heroapi,
+  popularsales,
+  toprateslaes,
+  highlight,
+  sneaker,
+} from "./data/data";
 
 const App = () => {
   return (
     <div className="flex flex-col gap-16 relative">
       <Hero heroapi={heroapi} />
-      <Sales endpoint={popularsales} />
+      <Sales endpoint={popularsales} ifExists />
+      <FlexContent endpoint={highlight} ifExists />
       <Sales endpoint={toprateslaes} />
+      <FlexContent endpoint={sneaker} />
     </div>
   );
 };
