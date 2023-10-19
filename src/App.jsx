356ca4e8d9a15aac6e80962b5ffbbefbec420f7d
Dhanraj Singh <sdhanraj300@gmail.com>
2023-10-19 12:47:03 +0530

    added Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FlexContent, Hero, Sales, Stories } from "./components";
+import { FlexContent, Footer, Hero, Sales, Stories } from "./components";
 import {
   heroapi,
   popularsales,
@@ -7,17 +7,21 @@ import {
   highlight,
   sneaker,
   story,
+  footerAPI,
 } from "./data/data";
 
 const App = () => {
   return (
-    <div className="flex flex-col gap-16 relative">
-      <Hero heroapi={heroapi} />
-      <Sales endpoint={popularsales} ifExists />
-      <FlexContent endpoint={highlight} ifExists />
-      <Sales endpoint={toprateslaes} />
-      <FlexContent endpoint={sneaker} />
-      <Stories story={story} />
+    <div className="">
+      <div className="flex flex-col gap-16 relative">
+        <Hero heroapi={heroapi} />
+        <Sales endpoint={popularsales} ifExists />
+        <FlexContent endpoint={highlight} ifExists />
+        <Sales endpoint={toprateslaes} />
+        <FlexContent endpoint={sneaker} />
+        <Stories story={story} />
+      </div>
+      <Footer footerAPI={footerAPI} />
     </div>
   );
 };
