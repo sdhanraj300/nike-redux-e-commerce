4e43228a2cc60686ebbfd12c0e3fb360a99c26e1
Dhanraj Singh <sdhanraj300@gmail.com>
2023-10-19 16:04:29 +0530

    added cart ui

---
+++ src/components/cart/CartCount.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { ChevronDoubleLeftIcon, XMarkIcon } from "@heroicons/react/24/solid";
+const CartCount = () => {
+  return (
+    <div>
+      <div className="bg-white h-11 flex items-center justify-between px-3 sticky top-0 left-0 right-0 w-full ">
+        <div className="flex items-center gap-3">
+          <div className="gird items-center cursor-pointer">
+            <ChevronDoubleLeftIcon className="icon-style w-5 h-5 text-slate-900 hover:text-orange-500
+            stroke-[2]
+            " />
+          </div>
+          <div className="gird items-center">
+            <h1 className="text-base font-medium text-slate-900 ">
+              Your Cart<span className="bg-theme-cart rounded px-1 py-0.5 text-slate-100 text-xs font-normal">(items)</span>
+            </h1>
+          </div>
+        </div>
+        <div className="flex items-center ">
+          <button type="button" className="rounded bg-theme-cart active:scale-90 p-0.5">
+            <XMarkIcon className="stroke-[2] w-5 h-5 text-white " />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartCount;
---
+++ src/components/cart/CartEmpty.jsx
@@ -0,0 +1,27 @@
+import { ArrowLeftIcon } from "@heroicons/react/24/solid";
+import React from "react";
+import emptyBag from "../../assets/emptybag.png";
+const CartEmpty = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center flex-col h-screen px-11 text-center gap-7 ">
+        <img
+          src={emptyBag}
+          alt=""
+          className="w-40 lg:w-36 sm:w-28
+        h-auto object-fill transition-all duration-300 hover:scale-110
+        "
+        />
+        <button
+          className="button-theme bg-gradient-to-b from-amber-500 to-orange-500 shadow-lg shadow-orange-500 flex items-center justify-center text-slate-900  py-2 gap-3 text-sm px-5 font-semibold active:scale-110"
+          type="button"
+        >
+          <ArrowLeftIcon className="h-5 w-5 text-slate-900" />
+          <span className="">Back To Nike Store</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CartEmpty;
